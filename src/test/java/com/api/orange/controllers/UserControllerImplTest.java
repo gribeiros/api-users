da5d58cd136af2694e5aa8f3e17743e5e7a5e21c
Gustavo Ribeiro Silva <gustavors071998@gmail.com>
2022-01-11 08:57:31 -0300

    change application properties for docker

--- src/test/java/com/api/orange/controllers/UserControllerImplTest.java
+++ src/test/java/com/api/orange/controllers/UserControllerImplTest.java
@@ -2,13 +2,25 @@ package com.api.orange.controllers;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserControllerImplTest {
 
+    @BeforeEach
+    public void setup() {
+        System.out.println("Before");
+    }
+
+    @AfterEach()
+    public void afterSetup() {
+        System.out.println("After");
+    }
 
     @Test
-    void addition() {
+    void Test() {
         assertEquals(2, 2);
     }
 
